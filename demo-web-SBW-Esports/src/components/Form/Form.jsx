2cf509790cab091f804e0fdaf74e5009189d4be5
pong <156582011+PONGSAPAK-BOONSONTHI@users.noreply.github.com>
2024-05-27 01:58:50 +0700

    27/05/24

--- demo-web-SBW-Esports/src/components/Form/Form.jsx
+++ demo-web-SBW-Esports/src/components/Form/Form.jsx
@@ -45,16 +45,17 @@ const Form = () => {
       <div className={styles.Form}>
         <div className={styles.mainForm}>
 
-          <div className={styles.title}>
+          <div className={styles.head_title}>
             <h1>ลงทะเบียน</h1>
           </div>
 
-          <form ref={formRef} onSubmit={handleSubmit} name="google-sheet">
-            <div>
-              <label htmlFor='name'>ผู้ลงสมัครคนที่ N</label>
-              <div className={styles.input}>
-                  <label htmlFor='name'>คำนำหน้า</label>
-                <select>
+          <form className={styles.main_sention_Forn} ref={formRef} onSubmit={handleSubmit} name="google-sheet">
+            <h1 className={styles.title}>ผู้ลงสมัครคนที่ N</h1>
+            <div className={styles.form_Sention}>
+              <div className={styles.select}>
+                <label htmlFor='prefix'>คำนำหน้า</label>
+                <select id='prefix'>
+                  <option value="">-</option>
                   <option value="">เด็กชาย</option>
                   <option value="">เด็กหญิง</option>
                   <option value="">นาย</option>
@@ -64,26 +65,82 @@ const Form = () => {
 
               <div className={styles.input}>
                 <label htmlFor='name'>ชื่อ</label>
-                <input type="text" name="name" />
+                <input type="text" id='name' name="name" />
               </div>
 
               <div className={styles.input}>
-                <label htmlFor='game'>นามสกุล</label>
-                <input type="text" name="game" />
+                <label htmlFor='surname'>นามสกุล</label>
+                <input type="text" id="surname" name="game" />
               </div>
             </div>
 
-            <div className={styles.input}>
-              <label htmlFor='phone'>เบอร์</label>
-              <input type="text" name="phone" />
+            <div className={styles.form_Sention}>
+              <div className={styles.input}>
+                <label for="date">วัน/เดือน/ปีเกิด</label>
+                <input type="date" id="date" name="date" />
+              </div>
+
+              <div className={styles.input}>
+                <label htmlFor='phone'>เบอร์</label>
+                <input type="text" id='phone' name="phone" />
+              </div>
+              <div className={styles.input}>
+                <label htmlFor='room'>ชั้นเเละห้อง</label>
+                <input type="text" id='roon' name="room" />
+              </div>
+
+              <div className={styles.input}>
+                <label htmlFor='number&chapter'>เลขที่เเละตอน</label>
+                <input type="text" id='umber&chapter' name="umber&chapter" />
+              </div>
             </div>
-            <div className={styles.input}>
-              <label for="date">กรุณาgเลือก วัน/เดือน/ปีเกิด :</label>
-              <input type="date" id="date" name="date" />
+
+            <div className={styles.form_Sention}>
+              <div className={styles.input}>
+                <label htmlFor="name_game">ชื่อในเกม</label>
+                <input type="text" id='name_game' name="name_game" />
+              </div>
+
+              <div className={styles.input}>
+                <label htmlFor="openID">OpenID</label>
+                <input type="text" id='openID' name="openID" />
+              </div>
             </div>
+
+            <div className={styles.position_Sention}>
+              <label for="date">ตำแหน่งในเกม</label>
+
+              <div className={styles.category_select_position}>
+                <div className={styles.position}>
+                  <input type="radio" name="position" id="dsl" />
+                  <label htmlFor="dsl">DSL (ออฟเลน, เลนดาร์ค)</label>
+                </div>
+                <div className={styles.position}>
+                  <input type="radio" name="position" id="jug" />
+                  <label htmlFor="jug">JUG (ป่า, ฟาร์ม)</label>
+                </div>
+                <div className={styles.position}>
+                  <input type="radio" name="position" id="mid" />
+                  <label htmlFor="mid">MID (เลนกลาง)</label>
+                </div>
+                <div className={styles.position}>
+                  <input type="radio" name="position" id="adl" />
+                  <label htmlFor="adl">ADL (แครี่, เลนมังกร)</label>
+                </div>
+                <div className={styles.position}>
+                  <input type="radio" name="position" id="sup" />
+                  <label htmlFor="sup">SUP (ซัพพอร์ต, โรมมิ่ง)</label>
+                </div>
+              </div>
+
+
+            </div>
+
             <br />
-            <div className={styles.input}>
-              <input className={styles.Button} type="submit" value={loading ? "Loading..." : "ส่งคำตอบ"} />
+
+            <div className={styles.Button_sention}>
+              <a className={styles.Button}>ถัดไป</a>
+              {/* <input className={styles.Button} type="submit" value={loading ? "Loading..." : "ส่งคำตอบ"} /> */}
             </div>
           </form>
 
